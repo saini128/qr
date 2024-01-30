75b030a355a3f2d4074e6da39ee3a09bac29223c
Hushraj Singh <saini.hck@gmail.com>
2024-01-31 01:49:08 +0530

    js complete

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let qrcode = new QRCode(document.querySelector(".qrcode"));
 
 function updateQrCode(text) {
     if (text.trim() === "") {
-        
-        qrcode.makeCode("Default Value");
+        qrcode.makeCode("Input de BSDK!!");
     } else {
         qrcode.makeCode(text);
     }
@@ -16,18 +15,37 @@ function generateQr() {
 
 function downloadQr() {
     let canvas = document.querySelector(".qrcode canvas");
-    let imageURI = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    let borderSize = 15;
+
+    
+    let borderedCanvas = document.createElement("canvas");
+    borderedCanvas.width = canvas.width + 2 * borderSize;
+    borderedCanvas.height = canvas.height + 2 * borderSize;
+
+    let ctx = borderedCanvas.getContext("2d");
+
+    
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, borderedCanvas.width, borderedCanvas.height);
+
+    
+    ctx.drawImage(canvas, borderSize, borderSize, canvas.width, canvas.height);
+
+    
+    let imageURI = borderedCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
+    
     let link = document.createElement("a");
     link.href = imageURI;
     link.download = "qrcode.png";
+
+    
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 }
 
-
-updateQrCode("Default Value");
-
+updateQrCode("Input de BSDK!!");
 
 document.querySelector("input").addEventListener("input", function() {
     generateQr();
